81daa4e512f87bf18ec9d82ffd51cf5b6e86571f
Antonio <antonio18casta@aol.com>
2022-08-19 14:13:58 -0400

    commit tree aa388af7fa05276332f819b6bafadf60045268b8

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(sentence) {
+    return sentence.split("").map(s => {
+
+      return (/([a-zA-Z]|\s|-|')/.test(s)) ? s : ""
+
+
+    }).join("")
+  }
+
+  static titleize(sentence) {
+    const arr = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let modSentence = sentence.split(" ").map(s => {
+      if(!arr.includes(s)) {
+        return Formatter.capitalize(s)
+      } else {
+        return s
+      }
+
+    }).join(" ")
+    
+    return Formatter.capitalize(modSentence)
+  }
+};
